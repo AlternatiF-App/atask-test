b4afc890cb627b5259a03e53cd747e636cf74e10
AlternatiF-App <fananiapps@gmail.com>
2025-06-18 22:32:49 +0700

    feat(feat-github): Create github search

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,121 @@
-import Image from "next/image";
+"use client"
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useDebounce } from "use-debounce";
+import { Collapse, CollapseProps, Input } from "antd";
+import { GitHubData, GitHubRepoOwner, GitHubRepository } from "./types/github.type";
+import { GiRoundStar } from "react-icons/gi";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+  const [username, setUsername] = useState<string>("")
+  const [value] = useDebounce(username, 1000);
+  const [data, setData] = useState<GitHubData[]>([])
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
+  const fetchAPI = async () => {
+    if (!username) {
+      setData([]);
+      return;
+    }
+
+    await axios.get(`https://api.github.com/search/users?q=${username}&per_page=5`)
+      .then(async (results) => {
+        const users = results.data.items;
+
+        const userRepoData = await Promise.all(
+          users.map(async (user: GitHubRepoOwner) => {
+            try {
+              const repoRes = await axios.get(`https://api.github.com/users/${user.login}/repos?per_page=3`);
+              return {
+                user,
+                repos: repoRes.data
+              };
+            } catch (err) {
+              console.error(`Error fetching repos for ${user.login}`, err);
+              return {
+                user,
+                repos: []
+              };
+            }
+          })
+        );
+
+        setData(userRepoData);
+      })
+      .catch(() => {
+        setData([])
+      })
+  }
+
+  useEffect(() => {
+    fetchAPI()
+  }, [value])
+
+  const items: CollapseProps['items'] = data.map((i: GitHubData, idx: number) => {
+    return {
+      key: idx + 1,
+      label: i.user.login,
+      children: 
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
+          {
+            i.repos.length !== 0
+              ? i.repos.map((r: GitHubRepository, ridx: number) => {
+                  return (
+                    <div key={ridx} className="px-4 py-2 border border-gray-200 rounded-lg">
+                      <div className="flex justify-between items-start">
+                        <div>
+                          <span className="block text-lg font-bold">{ r.name }</span>
+                          <span className="block truncate lg:max-w-60">{ r.description ?? "No description" }</span>
+                        </div>
+                        <div className="flex items-center space-x-1 min-w-8 text-end">
+                          <span className="block">{ r.stargazers_count ?? 0 }</span>
+                          <GiRoundStar className="h-4 w-4" />
+                        </div>
+                      </div>
+                    </div>
+                  )
+                })
+              : <span>No Repository</span>
+          }
+        </div>,
+    }
+  })
+
+  return (
+    <main className="container mx-auto px-4 py-2 2xl:px-20 2xl:py-10">
+      <div className="space-y-2">
+        <h1 className="font-bold text-xl">GitHub User and Repository</h1>
+        <Input
+          size="large"
+          placeholder="Search username"
+          allowClear
+          value={username}
+          onChange={(e) => {
+            if (e === undefined) {
+              setUsername("")
+            } else {
+              setUsername(e.target.value)
+            }
+          }}
+        />
+      </div>
+
+      <div className="mt-2 mb-6 text-gray-500 flex justify-between items-center">
+        <span className="block">Total <span className="font-bold text-black">{ data?.length }</span> items</span>
+        {
+          value !== "" && <span className="block">Showing users for <span className="font-bold text-black">{ value }</span></span>
+        }
+      </div>
+
+      <div>
+        {
+          data.length !== 0
+          ? <Collapse
+              items={items}
+              defaultActiveKey={['1']}
             />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+          : <span>No Items</span>
+        }
+      </div>
+    </main>
   );
 }
